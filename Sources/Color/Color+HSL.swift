327ee10c471b5fe3b63c9b2f4c7a9b9adc27cfe8
George Moschovitis <gmosx@reizu.com>
2018-05-12 16:10:30 +0300

    New hsl/hsv computation code

--- Sources/Color/Color+HSL.swift
+++ Sources/Color/Color+HSL.swift
@@ -1,131 +1,103 @@
 // http://en.wikipedia.org/wiki/HSL_color_space
 // https://gist.github.com/mjackson/5311256
-
-//fileprivate func hueToRGB(p: Float, q: Float, t: Float) -> Float {
-//    if t < 0 {
-//        t += 1
-//    }
-//
-//    if t > 1 {
-//        t -= 1
-//    }
-//
-//    if t < 1/6 {
-//        return p + (q - p) * 6 * t
-//    }
-//
-//    if t < (1/2) {
-//        return q
-//    }
-//
-//    if t < (2/3) {
-//        return p + (q - p) * (2/3 - t) * 6
-//    }
-//
-//    return p
-//}
+// https://github.com/dylan/colors/blob/master/Sources/Color%2BConversion.swift
 
 public extension Color {
-    public typealias HSL = (hue: Float, saturation: Float, lightness: Float)
-    public typealias HSLA = (hue: Float, saturation: Float, lightness: Float, alpha: Float)
+    public typealias HSLComponents = (hue: Float, saturation: Float, lightness: Float)
+    public typealias HSVComponents = (hue: Float, saturation: Float, value: Float)
 
 //    public init(hue: Float, saturation: Float, lightness: Float, alpha: Float = 1) {
-////        var r, g, b;
-////
-////        if (s == 0) {
-////        r = g = b = l; // achromatic
-////        } else {
-////        function hue2rgb(p, q, t) {
-////        if (t < 0) t += 1;
-////        if (t > 1) t -= 1;
-////        if (t < 1/6) return p + (q - p) * 6 * t;
-////        if (t < 1/2) return q;
-////        if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-////        return p;
-////        }
-////
-////        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-////        var p = 2 * l - q;
-////
-////        r = hue2rgb(p, q, h + 1/3);
-////        g = hue2rgb(p, q, h);
-////        b = hue2rgb(p, q, h - 1/3);
-////        }
-////
-////        return [ r * 255, g * 255, b * 255 ];
-//
-//        self.alpha = alpha
-//
-//        if saturation == 0 {
-//            self.red = 1
-//            self.green = 1
-//            self.blue = 1
-//        } else {
-//            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-//            var p = 2 * l - q;
-//        }
 //    }
 
-    // L = (M + m) / 2
-    public var hsl: HSL {
-        let maximum = max(red, green, blue)
-        let minimum = min(red, green, blue)
-        let chroma = maximum - minimum
+    public var hsl: HSLComponents {
+        let min = Swift.min(red, green, blue)
+        let max = Swift.max(red, green, blue)
+        let chroma = max - min
 
-        let hue: Float
-        let saturation: Float
-        let lightness: Float
+        var h: Float = 0
+        var s: Float = 0
+        var l: Float = 0
 
-        lightness = (maximum + minimum) / 2
-
-        if chroma == 0 {
-            hue = 0.0
-            saturation = 0.0
-        } else {
-            saturation = lightness > 0.5 ? chroma / (2 - maximum - minimum) : chroma / (maximum + minimum)
-
-            switch maximum {
-            case red:
-                hue = (green - blue) / chroma + (green < blue ? 6 : 0)
-
-            case green:
-                hue = (blue - red) / chroma + 2
-
-            case green:
-                hue = (red - green) / chroma + 4
-
-            default:
-                fatalError()
-            }
+        if max == min {
+            h = 0
+        } else if max == red {
+            h = (green - blue) / chroma
+        } else if max == green {
+            h = 2 + (blue - red) / chroma
+        } else if max == blue {
+            h = 4 + (red - green) / chroma
         }
 
-//        let normalizedHue = hue / 60
-//        let scaledHue = normalizedHue * 360
-        
-        return (hue: hue * 60, saturation: saturation, lightness: lightness) // returns hue in degrees [0..360]
-    }
-    
-    public var hsla: HSLA {
-        let tripplet = self.hsl
-        return (hue: tripplet.hue, saturation: tripplet.saturation, lightness: tripplet.lightness, alpha: rgba.alpha)
+        h = Swift.min(h * 60, 360)
+
+        if h > 360 {
+            h -= 360
+        }
+
+        if h < 0 {
+            h += 360
+        }
+
+        l = (min + max) / 2
+
+        if max == min {
+            s = 0
+        } else if l <= 0.5 {
+            s = chroma / (max + min)
+        } else {
+            s = chroma / (2 - max - min)
+        }
+
+        return HSLComponents(hue: h, saturation: s, lightness: l) // * 100?
     }
 
-    public var hue: Float {
-        return 1 // TODO: implement me!
+    public var hsv: HSVComponents {
+        let min = Swift.min(red, green, blue)
+        let max = Swift.max(red, green, blue)
+        let chroma = max - min
+
+        var h: Float = 0
+        var s: Float = 0
+        var v: Float = 0
+
+        if max == min {
+            s = 0
+        } else {
+            s = chroma / max
+        }
+
+        if max == min {
+            h = 0
+        } else if max == red {
+            h = (green - blue) / chroma
+        } else if max == green {
+            h = 2 + (blue - red) / chroma
+        } else if max == blue {
+            h = 4 + (red - green) / chroma
+        }
+
+        h = Swift.min(h * 60, 360)
+
+        if h < 0 {
+            h += 360
+        }
+
+        v = max
+
+        return HSVComponents(hue: h, saturation: s, value: v)
     }
 
-    public var saturation: Float {
-        return 1 // TODO: implement me!
-    }
+    public var chroma: Float {
+        let min = Swift.min(red, green, blue)
+        let max = Swift.max(red, green, blue)
 
-    public var lightness: Float {
-        return 1 // TODO: implement me!
+        return max - min
     }
 
     public var luma: Float {
         return (0.299 * red) + (0.587 * green) + (0.114 * blue)
     }
-    
+
     // TODO: move isDark here?
     // TODO: add isLight
 }
--- Tests/ColorTests/ColorTests.swift
+++ Tests/ColorTests/ColorTests.swift
@@ -11,13 +11,6 @@ class ColorTests: XCTestCase {
         XCTAssertEqual(hsl.hue, 300)
         XCTAssertEqual(hsl.saturation, 1.0)
         XCTAssertEqual(hsl.lightness, 0.25)
-
-        let hsla = c.with(alpha: 0.5).hsla
-        
-        XCTAssertEqual(hsla.hue, 300)
-        XCTAssertEqual(hsla.saturation, 1.0)
-        XCTAssertEqual(hsla.lightness, 0.25)
-        XCTAssertEqual(hsla.alpha, 0.5)
     }
 
     static var allTests = [
